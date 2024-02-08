8a6a4c567afe3281dcd72140049a24d3c6fb0e72
Alan Ramalho <azz_alan@hotmail.com>
2024-02-08 13:13:25 -0300

    script to push pending changes to wip branch

---
+++ git_push_to_wip.ps1
@@ -0,0 +1,14 @@
+# Check if the wip branch exists locally
+$localExists = git branch --list wip
+
+# Check if the wip branch exists on the remote
+$remoteExists = git ls-remote --heads origin wip
+
+if ($localExists -or $remoteExists) {
+    Write-Host "The 'wip' branch already exists either locally or on the remote. Exiting..."
+} else {
+    git checkout -b wip;
+    git stage --all;
+    git commit -m "wip";
+    git push origin wip
+}
